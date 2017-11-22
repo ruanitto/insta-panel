12ba1ac3b64b36108048ecaab8cc62c600d6e5b9
Rafael Gomes <rafael.gomes@madsolutions.com.br>
2017-11-22 20:58:31 -0200

    Update for WebApp

--- defaults/preferences/prefs.js
+++
@@ -1,9 +0,0 @@
-pref("extensions.instagram_panel.panelWidth", 50);
-pref("extensions.instagram_panel.panelHeight", 90);
-pref("extensions.instagram_panel.isFirstRun", true);
-pref("extensions.instagram_panel.delayFirstRun", 10);
-pref("extensions.instagram_panel.modfiers", "shift alt");
-pref("extensions.instagram_panel.key", "i");
-pref("extensions.instagram_panel.noautohide", false);
-pref("extensions.instagram_panel.toolbarAutoHide", false);
-pref("extensions.instagram_panel.faviconRefreshTime", 1);
